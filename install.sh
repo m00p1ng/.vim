6ad672372a2ed9b4af4f17d1e357bd59542fab8a
m00p1ng <mongkonchai4412@gmail.com>
2021-02-07 18:14:02 +0700

    add nvim config

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "source ~/.vim/vim-config/main.vim" > ~/.vimrc
+mkdir -p ~/.config/nvim && cp ~/.vim/vim-config/init.vim ~/.config/nvim
 vim +PlugInstall +qall
