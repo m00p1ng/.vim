db7ddf32c5deb49168f7b35c3cf5f48bc6dada20
m00p1ng <mongkonchai4412@gmail.com>
2021-02-10 04:48:54 +0700

    rename main -> init

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "source ~/.vim/vim-config/main.vim" > ~/.vimrc
+echo "source ~/.vim/vim-config/init.vim" > ~/.vimrc
 mkdir -p ~/.config/nvim && cp ~/.vim/vim-config/init.vim ~/.config/nvim
 vim +PlugInstall +qall
